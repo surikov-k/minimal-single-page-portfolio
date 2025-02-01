9b55188415cf212c3f27b95e4c57586fe3d142e3
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2025-02-01 23:26:32 +0300

    Intro Section

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,7 +1,16 @@
 import { FC } from "react";
 
 const Intro: FC = () => {
-  return <div>Intro</div>;
+  return (
+    <section className="mt-12 py-24 md:mt-16 md:py-32 lg:mt-20 lg:py-40">
+      <div className="container">
+        <h2 className="text-4xl md:text-7xl lg:w-[80%] lg:text-8xl">
+          Building beautiful websites with clean code and thoughtful design to
+          help your business grow and stand out online
+        </h2>
+      </div>
+    </section>
+  );
 };
 
 export default Intro;
