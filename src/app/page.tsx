f4fe69c9698c26b61281f6cbdc10dc4ac5b989c1
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2025-02-02 22:53:58 +0300

    Projects Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/sections/Header";
 import Hero from "@/sections/Hero";
 import Intro from "@/sections/Intro";
+import Projects from "@/sections/Projects";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <Intro />
+      <Projects />
     </>
   );
 }
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,4 +1,6 @@
+import Image from "next/image";
 import { FC } from "react";
+
 import image1 from "@/assets/images/project-1.jpg";
 import image2 from "@/assets/images/project-2.jpg";
 import image3 from "@/assets/images/project-3.jpg";
@@ -30,7 +32,49 @@ const projects = [
 ];
 
 const Projects: FC = () => {
-  return <div>Projects</div>;
+  return (
+    <section className="py-24 md:py-32 lg:py-40">
+      <div className="container">
+        <h2 className="text-4xl md:text-7xl lg:text-8xl">Selected works</h2>
+        <div className="mt-10 md:mt-16 lg:mt-20">
+          {projects.map(({ name, image }) => (
+            <a
+              href="#"
+              key={name}
+              className="flex flex-col border-t border-dotted border-stone-400 py-6 last:border-b md:py-8 lg:py-10"
+            >
+              <div>
+                <div className="aspect-video md:hidden">
+                  <Image
+                    src={image}
+                    alt={`${name} image`}
+                    className="size-full object-cover"
+                  />
+                </div>
+                <div className="mt-8 flex items-center justify-between md:mt-0">
+                  <h3 className="text-2xl md:text-3xl lg:text-4xl">{name}</h3>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </a>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
