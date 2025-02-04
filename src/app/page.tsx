456a7cc031dc843ce6df79771f9765e072a24ee8
Konstantin Surikov <19418260+surikov-k@users.noreply.github.com>
2025-02-04 23:53:01 +0300

    Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import FAQs from "@/sections/FAQs";
+import Footer from "@/sections/Footer";
 import Header from "@/sections/Header";
 import Hero from "@/sections/Hero";
 import Intro from "@/sections/Intro";
@@ -14,6 +15,7 @@ export default function Home() {
       <Projects />
       <Testimonials />
       <FAQs />
+      <Footer />
     </>
   );
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,7 +1,88 @@
+import Link from "next/link";
 import { FC } from "react";
 
+import Button from "@/components/Button";
+
+const navItems = [
+  {
+    href: "#",
+    label: "Home",
+  },
+
+  {
+    href: "#",
+    label: "Projects",
+  },
+  {
+    href: "#",
+    label: "Testimonials",
+  },
+  {
+    href: "#",
+    label: "Faqs",
+  },
+  {
+    href: "#",
+    label: "Contact",
+  },
+];
+
 const Footer: FC = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="bg-stone-900 text-white">
+      <div className="container">
+        <div className="section">
+          <div className="flex items-center gap-3">
+            <div className="size-3 rounded-full bg-green-400"></div>
+            <span className="uppercase">One spot available for next month</span>
+          </div>
+          <div className="md:grid md:grid-cols-3 md:items-center">
+            <div className="md:col-span-2">
+              <h2 className="mt-8 text-4xl font-extralight md:text-7xl lg:text-8xl">
+                Enough talk. Let&apos;s make something great together.
+              </h2>
+              <Button
+                className="mt-8"
+                variant="secondary"
+                iconAfter={
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="size-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+                    />
+                  </svg>
+                }
+              >
+                info@alextailor.com
+              </Button>
+            </div>
+            <div className="">
+              <nav className="mt-16 flex flex-col items-end gap-8 md:mt-0">
+                {navItems.map(({ href, label }) => (
+                  <Link key={label} href={href}>
+                    <Button variant="text" className="text-lg">
+                      {label}
+                    </Button>
+                  </Link>
+                ))}
+              </nav>
+            </div>
+          </div>
+        </div>
+        <p className="py-16 text-sm text-white/30">
+          Copyright &copy; Alex Taylor &bull; All rights reserved
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
